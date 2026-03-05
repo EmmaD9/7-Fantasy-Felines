f437f0f8a3a8fa2efd849e4efa6eac395e608fbf
maa1446 <maa1446@rit.edu>
2026-03-05 02:18:22 -0500

    Added logic and timer for stat changes

--- Assets/Scripts/StatManager.cs
+++ Assets/Scripts/StatManager.cs
@@ -4,25 +4,160 @@ using UnityEngine.UI;
 public class StatManager : MonoBehaviour
 {
     // Field Declaration
-    public Image foodStat;
-    public Image washStat;
-    public Image exerciseStat;
+    public Image foodImage;
+    public Image washImage;
+    public Image exerciseImage;
 
+    private int foodStat;
+    private int washStat;
+    private int exerciseStat;
+
+    public int foodStatMax;
+    public int washStatMax;
+    public int exerciseStatMax;
+
+    public float statDecreaseTimer;
+    private float timeRemaining;
+    private bool timerIsRunning;
+
+
+    void Start()
+    {
+        // Initialize stats to max
+        foodStat = foodStatMax;
+        washStat = washStatMax;
+        exerciseStat = exerciseStatMax;
+
+        // Initialize stat colors to green
+        foodImage.color = Color.green;
+        washImage.color = Color.green;
+        exerciseImage.color = Color.green;
+
+        timeRemaining = statDecreaseTimer;
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        // Logic for the timer
+        if (timerIsRunning)
+        {
+            // If time is left, keep decrementing time
+            // Otherwise, decrease all stats, update colors,
+            // and reset timer
+            if(timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                foodStat--;
+                if(foodStat < 0)
+                {
+                    foodStat = 0;
+                }
+                FeedUpdate();
+                washStat--;
+                if (washStat < 0)
+                {
+                    washStat = 0;
+                }
+                WashUpdate();
+                exerciseStat--;
+                if (exerciseStat < 0)
+                {
+                    exerciseStat = 0;
+                }
+                ExerciseUpdate();
+                timeRemaining = statDecreaseTimer;
+            }
+        }
+    }
 
     // Public methods to link with buttons
     public void OnFeedClick()
     {
-        Debug.Log("Nom Nom Nom!");
-        foodStat.color = Color.green;
+        foodStat++;
+        FeedUpdate();
     }
     public void OnWashClick()
     {
-        Debug.Log("Squeaky Clean!");
-        washStat.color = Color.green;
+        washStat++;
+        WashUpdate();
     }
     public void OnExerciseClick()
     {
-        Debug.Log("Whew! I'm tired!");
-        exerciseStat.color = Color.red;
+        exerciseStat++;
+        ExerciseUpdate();
+    }
+
+    // Methods to update stat color
+    public void FeedUpdate()
+    {
+        // Cap number at max for stat
+        if (foodStat > foodStatMax)
+        {
+            foodStat = foodStatMax;
+        }
+
+        // Change color according to current stat
+        if (foodStat < 7)
+        {
+            foodImage.color = Color.red;
+        }
+        else if (foodStat > 7 && foodStat < 15)
+        {
+            foodImage.color = Color.yellow;
+        }
+        else if (foodStat > 15)
+        {
+            foodImage.color = Color.green;
+        }
+    }
+
+    public void WashUpdate()
+    {
+        // Cap number at max for stat
+        if (washStat > washStatMax)
+        {
+            washStat = washStatMax;
+        }
+
+        // Change color according to current stat
+        if (washStat < 7)
+        {
+            washImage.color = Color.red;
+        }
+        else if (washStat > 7 && washStat < 15)
+        {
+            washImage.color = Color.yellow;
+        }
+        else if (washStat > 15)
+        {
+            washImage.color = Color.green;
+        }
+    }
+
+    public void ExerciseUpdate()
+    {
+        // Cap number at max for stat
+        if (exerciseStat > exerciseStatMax)
+        {
+            exerciseStat = exerciseStatMax;
+        }
+
+        // Change color according to current stat
+        if (exerciseStat < 7)
+        {
+            exerciseImage.color = Color.red;
+        }
+        else if (exerciseStat > 7 && exerciseStat < 15)
+        {
+            exerciseImage.color = Color.yellow;
+        }
+        else if (exerciseStat > 15)
+        {
+            exerciseImage.color = Color.green;
+        }
     }
 }
