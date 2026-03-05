cb323c915ca4986776e7c2e319fc8cf87011693e
maa1446 <maa1446@rit.edu>
2026-03-04 22:19:07 -0500

    Initial Button Interactivity set up

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void OnFeedClick()
+    {
+        Debug.Log("Nom Nom Nom!");
+    }
+    public void OnWashClick()
+    {
+        Debug.Log("Squeaky Clean!");
+    }
+    public void OnExerciseClick()
+    {
+        Debug.Log("Whew! I'm tired!");
+    }
+}
